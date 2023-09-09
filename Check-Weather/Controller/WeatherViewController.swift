ce7b68230411bc4c055c851a660d77e421cb33af
AwesomeJim <jimnahgua@gmail.com>
2023-09-09 13:48:10 +0300

    - Set up TextField

--- Check-Weather/Controller/WeatherViewController.swift
+++ Check-Weather/Controller/WeatherViewController.swift
@@ -8,17 +8,49 @@
 
 import UIKit
 
-class WeatherViewController: UIViewController {
+class WeatherViewController: UIViewController, UITextFieldDelegate {
 
     @IBOutlet weak var conditionImageView: UIImageView!
     @IBOutlet weak var temperatureLabel: UILabel!
     @IBOutlet weak var cityLabel: UILabel!
     
+    @IBOutlet weak var searchTextField: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        searchTextField.delegate = self // report back to viewController via callback
     }
 
-
+    @IBAction func searchPressed(_ sender: UIButton) {
+        searchTextField.endEditing(true)
+       
+    }
+    
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        searchTextField.endEditing(true)
+        return true
+    }
+    
+    
+    func textFieldDidEndEditing(_ textField: UITextField) {
+        guard let searchLocation = searchTextField.text else {
+            //searchTextField.er
+            return
+        }
+        print(searchLocation)
+        searchTextField.text = ""
+    }
+    
+    
+    func textFieldShouldEndEditing(_ textField: UITextField) -> Bool {
+        if textField.text != ""{
+            return true
+        }else {
+            searchTextField.placeholder = "Please type location"
+            return false
+        }
+    }
+    
 }
 
